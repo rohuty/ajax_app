970ae52d4332e0196bae1e2a01d2c122cf3c48cc
rohuty <rohuty0601@yahoo.co.jp>
2020-10-07 22:28:50 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   def index  # indexアクションを定義した
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC") # 新しいメモが一番上に表示される
   end
 
-  def new
-    
-  end
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index # メモを保存した後にトップページへリダイレクトされるように追記
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   #[HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]'
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  # get 'posts', to: 'posts#index' ←編集して下記へ変更
+  root to: 'posts#index' # ルートパスへ変更
+  # get 'posts/new', to: 'posts#new' ←削除
+  #「投稿完了しました」というページは使用しないため、get 'posts/new', to: 'posts#new'の記述は削除
   post 'posts', to: 'posts#create'
 
 end
